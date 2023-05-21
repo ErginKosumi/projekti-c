9abad047619215e187aefa9bd0d5e1a923cb3bd6
Ergin Kosumi <113962607+ErginKosumi@users.noreply.github.com>
2023-05-21 23:23:44 +0200

    Add files via upload

---
+++ arraymin/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniprojekti
+{
+    internal class Program
+    {
+        public class ArrayMin
+        {
+            public static void Main()
+            {
+                Console.WriteLine("Vendosni madhesine e array:");
+                int size = int.Parse(Console.ReadLine());
+
+                int[] array = new int[size];
+
+                Console.WriteLine("Vendosni elementet e array:");
+                for (int i = 0; i < size; i++)
+                {
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+
+                int minValue = Min(array);
+                Console.WriteLine("Vlera minimale e array eshte: {0}", minValue);
+            }
+
+            public static int Min(int[] array)
+            {
+                int minValue = array[0];
+                for (int i = 1; i < array.Length; i++)
+                {
+                    if (array[i] < minValue)
+                    {
+                        minValue = array[i];
+                    }
+                }
+                return minValue;
+            }
+        }
+    }
+}
